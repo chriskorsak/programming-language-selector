e93acb3fe8830b9d5b09f29d73802ea3039bb6ed
chriskorsak <chris@chriskorsak.com>
2020-05-04 18:21:24 -0700

    add variables to collect values from q's 6-9, implement add function to total q's 1-5, add branching logic, add output div .show() method to display output.

--- js/scripts.js
+++ js/scripts.js
@@ -8,14 +8,34 @@ function add(number1, number2, number3, number4, number5) {
 
 $(document).ready(function() {
   $("#questionaire").submit(function() {
-    var question1 = $("input:radio[name=question1]:checked").val();
-    var question2 = $("input:radio[name=question2]:checked").val();
-    var question3 = $("input:radio[name=question3]:checked").val();
-    var question4 = $("input:radio[name=question4]:checked").val();
-    var question5 = $("input:radio[name=question5]:checked").val();
-   
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
+    var question2 = parseInt($("input:radio[name=question2]:checked").val());
+    var question3 = parseInt($("input:radio[name=question3]:checked").val());
+    var question4 = parseInt($("input:radio[name=question4]:checked").val());
+    var question5 = parseInt($("input:radio[name=question5]:checked").val());
+    var question6 = $("#question6").val();
+    var question7 = $("#question7").val();
+    var question8 = parseInt($("#question8").val());
+    var question9 = $("#question9").val();
 
-    console.log(question1, question2, question3, question4, question5, question6, question7, question8, question9);
+    var radioTotal = add(question1, question2, question3, question4, question5);
+
+    if (radioTotal <= 5 && question7 === "false") {
+      $("#language").text("Swift");
+      $("#link").html('<a href="https://developer.apple.com/swift/">Swift</a>');
+    } else if (radioTotal <= 10 && question7 === "true") {
+      $("#language").text("C#");
+      $("#link").html('<a href="https://docs.microsoft.com/en-us/dotnet/csharp/">C#</a>');
+    } else if (question8 >= 2000) {
+      $("#language").text("Python");
+      $("#link").html('<a href="https://www.python.org/">Python</a>');
+    } else {
+      $("#language").text("Javascript");
+      $("#link").html('<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction">Javascript</a>');
+    }
+
+    $("#name").text(question9);
+    $("#output").show();
 
     event.preventDefault();
   });
